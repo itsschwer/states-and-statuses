e187aed7235f2b0e21ae17fb9508dc4f9f280886
Schwer <42115696+itsschwer@users.noreply.github.com>
2021-02-14 15:22:13 +1100

    Clean up DashlessOnTrigger.cs

--- Assets/Scripts/StatusTriggers/DashlessOnTrigger.cs
+++ Assets/Scripts/StatusTriggers/DashlessOnTrigger.cs
@@ -1,6 +1,4 @@
-﻿using UnityEngine;
-
-public class DashlessOnTrigger : StatusOnTrigger<IDashless> {
+﻿public class DashlessOnTrigger : StatusOnTrigger<IDashless> {
     protected override void Trigger(IDashless target) {
         target.dashless.Trigger();
     }
