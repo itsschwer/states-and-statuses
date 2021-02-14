63e20bb9d24663a70d252d742af6fddaac7fb6f0
Schwer <42115696+itsschwer@users.noreply.github.com>
2021-02-14 14:12:53 +1100

    Refactor MouseController.cs and Character.cs

--- Assets/Scripts/Actor/Character.cs
+++ Assets/Scripts/Actor/Character.cs
@@ -1,8 +1,16 @@
 ﻿using UnityEngine;
 
 public class Character : Actor, IGigantism {
+    [Header("Properties")]
+    [SerializeField] protected float moveSpeed = 10;
+    [SerializeField] protected float maxDashDistance = 5;
+
+    [Header("Inflictable Statuses")]
     [SerializeField] private Gigantism _gigantism = default;
     public Gigantism gigantism => _gigantism;
+
+    public float speedModifier { get; set; } = 1;
+    public bool canDash { get; set; } = true;
 
     private void OnValidate() {
         if (_gigantism == null) {
@@ -13,5 +21,16 @@ public class Character : Actor, IGigantism {
     protected override void Awake() {
         base.Awake();
         gigantism?.Initialise(this);
+    }
+
+    protected void MoveTowards(Vector2 destination, float maxDelta) {
+        var difference = destination - (Vector2)transform.position;
+        if (difference.magnitude > maxDelta) {
+            difference.Normalize();
+            transform.Translate(difference * maxDelta);
+        }
+        else {
+            transform.Translate(difference);
+        }
     }
 }
--- Assets/Scripts/MouseController.cs
+++ Assets/Scripts/MouseController.cs
@@ -1,35 +1,18 @@
 ﻿using UnityEngine;
 
-public class MouseController : MonoBehaviour {
-    [SerializeField] private float moveSpeed = 5;
-    [SerializeField] private float maxDashDistance = 3;
-    private new Transform transform;
+public class MouseController : Character {
     private new Camera camera;
 
     private Vector2 mousePosition => camera.ScreenToWorldPoint(Input.mousePosition);
 
-    private void Awake() {
-        transform = GetComponent<Transform>();
-        camera = Camera.main;
-    }
+    private void Start() => camera = Camera.main;
 
     private void Update() {
-        if (Input.GetMouseButtonDown(1)) {
-            MoveTowards(maxDashDistance);
+        if (Input.GetMouseButtonDown(1) && canDash) {
+            MoveTowards(mousePosition, maxDashDistance * speedModifier);
         }
         else if (Input.GetMouseButton(0)) {
-            MoveTowards(moveSpeed * Time.deltaTime);
-        }
-    }
-
-    private void MoveTowards(float maxDelta) {
-        var difference = mousePosition - (Vector2)transform.position;
-        if (difference.magnitude > maxDelta) {
-            difference.Normalize();
-            transform.Translate(difference * maxDelta);
-        }
-        else {
-            transform.Translate(difference);
+            MoveTowards(mousePosition, moveSpeed * speedModifier * Time.deltaTime);
         }
     }
 }
