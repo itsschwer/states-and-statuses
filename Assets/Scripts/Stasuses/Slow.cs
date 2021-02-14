3eb25b37df9b1d8754ee995c5a9e4e31fd279938
Schwer <42115696+itsschwer@users.noreply.github.com>
2021-02-14 15:32:15 +1100

    Add rudimentary color change to Slow
    Consider increasing color intensity per stack for more visual feedback. Also consider how this would interact with other color changing effects.

--- Assets/Scripts/Stasuses/Slow.cs
+++ Assets/Scripts/Stasuses/Slow.cs
@@ -2,6 +2,7 @@
 
 public class Slow : Status {
     [SerializeField] private float speedMultiplier = 0.5f;
+    [SerializeField] private Color colorChange = new Color(0.1850303f, 0.7667949f, 0.9339623f, 1);
     [SerializeField] private float duration = 3;
     [SerializeField] private int maxStacks = 8;
 
@@ -22,11 +23,15 @@ public class Slow : Status {
 
     private float[] timers;
 
+    private Color initialColor;
+
     private ISlow target;
 
     public void Initialise(ISlow target) {
         this.target = target;
         timers = new float[maxStacks];
+
+        initialColor = target.renderer.color;
     }
 
     public override void Trigger() {
@@ -41,6 +46,10 @@ public class Slow : Status {
             }
 
             target.speedModifier *= speedMultiplier;
+
+            if (target.renderer.color != colorChange) {
+                target.renderer.color = colorChange;
+            }
         }
 
         if (stacks > 0) {
@@ -59,6 +68,7 @@ public class Slow : Status {
             timers[i] = 0;
         }
 
+        target.renderer.color = initialColor;
         this.enabled = false;
     }
 
@@ -74,6 +84,7 @@ public class Slow : Status {
                 }
 
                 if (stacks <= 0) {
+                    target.renderer.color = initialColor;
                     this.enabled = false;
                 }
             }
